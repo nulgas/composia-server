ea1e003c039450988d695db3cf720786ba35f0a7
João Eliandro <eliandrogermano@gmail.com>
2020-10-31 18:14:59 -0300

    Created node server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 
+const api = require("./services/api");
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
+app.get('/', async (request, response) => {
+
+  const { data } = await api.get("/");
+
+  return response.json(data);
+});
+
+app.post('/test', async (request, response) => {
+  const body = request.body;  
+
+  const { data } = await api.post('/test', body);
+
+  return response.json(data);
 });
 
 app.listen(3333, () => {
