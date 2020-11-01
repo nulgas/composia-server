8030df1771dacb1c785c1640ba766228b3ec64d5
nulgas <62256915+nulgas@users.noreply.github.com>
2020-11-01 04:29:38 -0300

    Update api.py

--- src/scripts/api.py
+++ src/scripts/api.py
@@ -1,20 +1,27 @@
+#pip install -q aitextgen
+
+import json
+from aitextgen import aitextgen
 from flask import Flask, request
 
+
+# importa o modelo
+ai = aitextgen(model="models/pytorch_model_luiz_gonzaga.bin", config="models/config_luiz_gonzaga.json")
+
+
+
+# cria a API
 app = Flask("composia")
 
 
-@app.route("/", methods=["GET"])
-def helloWorld():
-    return{"Hello": "World"}
-
-
-@app.route("/test", methods=["POST"])
-def test():
-
-    body = request.get_json()
-    print(body)
-
-    return body
-
+@app.route("/estrofe", methods=["GET"])
+def estrofe():
+    return jsonify({'text': ai.generate_one(batch_size=100,
+                        prompt=request.args['input'],
+                        max_length=100,
+                        temperature=1.5,
+                        top_k=0,
+                        top_p=1)
+                      })
 
 app.run()
